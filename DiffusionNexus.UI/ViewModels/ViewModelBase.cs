4cdf6241f0a3c47b0eb35d990d49b9495af5d29a
Little-God <Little-God@web.de>
2025-06-05 14:01:48 +0200

    initial commit

---
+++ DiffusionNexus.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DiffusionNexus.UI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ DiffusionNexus.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DiffusionNexus.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
