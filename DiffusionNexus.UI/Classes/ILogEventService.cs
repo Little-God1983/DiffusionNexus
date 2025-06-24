d1b3454029b6b3bfe230ff6f935383b4602afb45
Chris <little-god@web.de>
2025-06-24 13:40:17 +0200

    compiles again

--- DiffusionNexus.UI/Classes/ILogEventService.cs
+++ DiffusionNexus.UI/Classes/ILogEventService.cs
@@ -1,9 +1,10 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using DiffusionNexus.UI.Models;
 
 namespace DiffusionNexus.UI.Classes
 {
-    public interface ILogEventService
+    public interface ILogEventService : INotifyPropertyChanged
     {
         ObservableCollection<LogEntry> Entries { get; }
         LogEntry? LatestEntry { get; }
