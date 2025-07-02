6032be04d45734bf9c9a54198fcf01f7476ebf84
Little-God1983 <33689122+Little-God1983@users.noreply.github.com>
2025-07-02 08:43:07 +0200

    Update LoraSortView.axaml.cs
    simplified logic

--- DiffusionNexus.UI/Views/LoraSortView.axaml.cs
+++ DiffusionNexus.UI/Views/LoraSortView.axaml.cs
@@ -9,9 +9,6 @@ namespace DiffusionNexus.UI.Views
 {
     public partial class LoraSortView : UserControl
     {
-        private LoraSortMainSettingsViewModel? _mainVm;
-        private LoraSortCustomMappingsViewModel? _mapVm;
-
         public LoraSortView()
         {
             InitializeComponent();
@@ -20,35 +17,8 @@ namespace DiffusionNexus.UI.Views
 
         private void OnAttached(object? sender, VisualTreeAttachmentEventArgs e)
         {
-            HookViewModels();
-            if (MainSettingsControl is not null)
-                MainSettingsControl.DataContextChanged += (_, _) => HookViewModels();
-            if (CustomMappingsControl is not null)
-                CustomMappingsControl.DataContextChanged += (_, _) => HookViewModels();
-        }
-
-        private void HookViewModels()
-        {
-            if (_mainVm is not null)
-                _mainVm.PropertyChanged -= MainVmOnPropertyChanged;
-
-            _mainVm = MainSettingsControl?.DataContext as LoraSortMainSettingsViewModel;
-            _mapVm = CustomMappingsControl?.DataContext as LoraSortCustomMappingsViewModel;
-
-            if (_mainVm is not null && _mapVm is not null)
-            {
-                _mapVm.IsCustomEnabled = _mainVm.UseCustomMappings;
-                _mainVm.PropertyChanged += MainVmOnPropertyChanged;
-            }
-        }
-
-        private void MainVmOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(LoraSortMainSettingsViewModel.UseCustomMappings))
-            {
-                if (_mainVm is not null && _mapVm is not null)
-                    _mapVm.IsCustomEnabled = _mainVm.UseCustomMappings;
-            }
+            // The coordination between view models is now handled in LoraSortViewModel
+            // so we don't need the manual property synchronization here anymore
         }
 
         private void InitializeComponent()
