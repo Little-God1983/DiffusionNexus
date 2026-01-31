3a8c0d6c7f65e20fd77b6c0d0ad8b378b181e4f2
Chris <little-god@web.de>
2026-02-01 00:40:53 +0100

    build in zoom

--- DiffusionNexus.UI/Views/Controls/ImageCompareControl.axaml.cs
+++ DiffusionNexus.UI/Views/Controls/ImageCompareControl.axaml.cs
@@ -10,6 +10,10 @@ namespace DiffusionNexus.UI.Views.Controls;
 
 public partial class ImageCompareControl : UserControl
 {
+    private const double MinZoom = 0.1;
+    private const double MaxZoom = 10.0;
+    private const double ZoomStep = 0.1;
+
     public static readonly StyledProperty<string?> BeforeImagePathProperty =
         AvaloniaProperty.Register<ImageCompareControl, string?>(nameof(BeforeImagePath));
 
@@ -22,11 +26,27 @@ public partial class ImageCompareControl : UserControl
     public static readonly StyledProperty<CompareFitMode> FitModeProperty =
         AvaloniaProperty.Register<ImageCompareControl, CompareFitMode>(nameof(FitMode), CompareFitMode.Fit);
 
+    public static readonly StyledProperty<double> ZoomLevelProperty =
+        AvaloniaProperty.Register<ImageCompareControl, double>(nameof(ZoomLevel), 1.0);
+
     private Control? _beforeContainer;
     private Control? _afterContainer;
     private Canvas? _overlayCanvas;
     private Border? _sliderLine;
     private Thumb? _sliderThumb;
+    private Image? _beforeImage;
+    private Image? _afterImage;
+
+    private double _panX;
+    private double _panY;
+    private Point _lastPanPoint;
+    private bool _isPanning;
+    private bool _isFitMode = true;
+
+    /// <summary>
+    /// Event raised when zoom level changes.
+    /// </summary>
+    public event EventHandler? ZoomChanged;
 
     public ImageCompareControl()
     {
@@ -37,11 +57,15 @@ public partial class ImageCompareControl : UserControl
         _overlayCanvas = this.FindControl<Canvas>("OverlayCanvas");
         _sliderLine = this.FindControl<Border>("SliderLine");
         _sliderThumb = this.FindControl<Thumb>("SliderThumb");
-
+        _beforeImage = this.FindControl<Image>("BeforeImageControl");
+        _afterImage = this.FindControl<Image>("AfterImageControl");
 
         if (_overlayCanvas is not null)
         {
             _overlayCanvas.PointerPressed += OnOverlayPointerPressed;
+            _overlayCanvas.PointerMoved += OnOverlayPointerMoved;
+            _overlayCanvas.PointerReleased += OnOverlayPointerReleased;
+            _overlayCanvas.PointerWheelChanged += OnOverlayPointerWheelChanged;
         }
 
         if (_sliderThumb is not null)
@@ -49,16 +73,18 @@ public partial class ImageCompareControl : UserControl
             _sliderThumb.DragDelta += OnSliderThumbDragDelta;
         }
 
-        PropertyChanged += (s, e) =>
-        {
-            if (e.Property == SliderValueProperty || e.Property == BoundsProperty)
-            {
-                UpdateVisuals();
-            }
-        };
+        PropertyChanged += OnControlPropertyChanged;
 
         // Update visuals when layout is complete
         LayoutUpdated += OnLayoutUpdated;
+    }
+
+    private void OnControlPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        if (e.Property == SliderValueProperty || e.Property == BoundsProperty || e.Property == ZoomLevelProperty)
+        {
+            UpdateVisuals();
+        }
     }
 
     private void OnLayoutUpdated(object? sender, EventArgs e)
@@ -90,6 +116,42 @@ public partial class ImageCompareControl : UserControl
         set => SetValue(FitModeProperty, value);
     }
 
+    public double ZoomLevel
+    {
+        get => GetValue(ZoomLevelProperty);
+        set
+        {
+            var clamped = Math.Clamp(value, MinZoom, MaxZoom);
+            SetValue(ZoomLevelProperty, clamped);
+            _isFitMode = false;
+            ZoomChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    /// <summary>
+    /// Gets the zoom level as a percentage (10-1000).
+    /// </summary>
+    public int ZoomPercentage => (int)Math.Round(ZoomLevel * 100);
+
+    /// <summary>
+    /// Gets or sets whether fit mode is active.
+    /// </summary>
+    public bool IsFitMode
+    {
+        get => _isFitMode;
+        set
+        {
+            _isFitMode = value;
+            if (value)
+            {
+                _panX = 0;
+                _panY = 0;
+            }
+            UpdateVisuals();
+            ZoomChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
     private void OnOverlayPointerPressed(object? sender, PointerPressedEventArgs e)
     {
         if (_overlayCanvas is null)
@@ -97,8 +159,59 @@ public partial class ImageCompareControl : UserControl
             return;
         }
 
+        var props = e.GetCurrentPoint(_overlayCanvas).Properties;
+
+        // Middle mouse button for panning
+        if (props.IsMiddleButtonPressed)
+        {
+            _isPanning = true;
+            _lastPanPoint = e.GetPosition(_overlayCanvas);
+            e.Handled = true;
+            return;
+        }
+
         var position = e.GetPosition(_overlayCanvas);
         SetSliderFromPosition(position.X);
+        e.Handled = true;
+    }
+
+    private void OnOverlayPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (_overlayCanvas is null || !_isPanning)
+        {
+            return;
+        }
+
+        var point = e.GetPosition(_overlayCanvas);
+        var deltaX = point.X - _lastPanPoint.X;
+        var deltaY = point.Y - _lastPanPoint.Y;
+
+        Pan(deltaX, deltaY);
+        _lastPanPoint = point;
+        e.Handled = true;
+    }
+
+    private void OnOverlayPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        if (_isPanning)
+        {
+            _isPanning = false;
+            e.Handled = true;
+        }
+    }
+
+    private void OnOverlayPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+    {
+        // Zoom with mouse wheel
+        if (e.Delta.Y > 0)
+        {
+            ZoomIn();
+        }
+        else if (e.Delta.Y < 0)
+        {
+            ZoomOut();
+        }
+
         e.Handled = true;
     }
 
@@ -140,6 +253,23 @@ public partial class ImageCompareControl : UserControl
             return;
         }
 
+        // Apply zoom transform to both image containers
+        var zoomTransform = new TransformGroup();
+        zoomTransform.Children.Add(new ScaleTransform(ZoomLevel, ZoomLevel));
+        zoomTransform.Children.Add(new TranslateTransform(_panX, _panY));
+
+        if (_beforeImage is not null)
+        {
+            _beforeImage.RenderTransformOrigin = new RelativePoint(0.5, 0.5, RelativeUnit.Relative);
+            _beforeImage.RenderTransform = zoomTransform;
+        }
+
+        if (_afterImage is not null)
+        {
+            _afterImage.RenderTransformOrigin = new RelativePoint(0.5, 0.5, RelativeUnit.Relative);
+            _afterImage.RenderTransform = zoomTransform;
+        }
+
         var sliderX = width * (SliderValue / 100d);
         
         var afterClipRect = new Rect(sliderX, 0, Math.Max(0, width - sliderX), height);
@@ -157,5 +287,64 @@ public partial class ImageCompareControl : UserControl
 
         Canvas.SetLeft(_sliderThumb, sliderX - (thumbWidth / 2));
         Canvas.SetTop(_sliderThumb, (height - thumbHeight) / 2);
+    }
+
+    /// <summary>
+    /// Increases the zoom level.
+    /// </summary>
+    public void ZoomIn() => ZoomLevel += ZoomStep;
+
+    /// <summary>
+    /// Decreases the zoom level.
+    /// </summary>
+    public void ZoomOut() => ZoomLevel -= ZoomStep;
+
+    /// <summary>
+    /// Sets the zoom level to fit the images within the control.
+    /// </summary>
+    public void ZoomToFit()
+    {
+        SetValue(ZoomLevelProperty, 1.0);
+        _panX = 0;
+        _panY = 0;
+        _isFitMode = true;
+        UpdateVisuals();
+        ZoomChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    /// <summary>
+    /// Sets the zoom level to 100% (actual size).
+    /// </summary>
+    public void ZoomToActual()
+    {
+        ZoomLevel = 1.0;
+        _panX = 0;
+        _panY = 0;
+    }
+
+    /// <summary>
+    /// Pans the images by the specified delta values.
+    /// </summary>
+    /// <param name="deltaX">The delta value for the X axis.</param>
+    /// <param name="deltaY">The delta value for the Y axis.</param>
+    public void Pan(double deltaX, double deltaY)
+    {
+        if (_isFitMode) return;
+        _panX += deltaX;
+        _panY += deltaY;
+        UpdateVisuals();
+    }
+
+    /// <summary>
+    /// Resets zoom to default state.
+    /// </summary>
+    public void ResetZoom()
+    {
+        SetValue(ZoomLevelProperty, 1.0);
+        _panX = 0;
+        _panY = 0;
+        _isFitMode = true;
+        UpdateVisuals();
+        ZoomChanged?.Invoke(this, EventArgs.Empty);
     }
 }
